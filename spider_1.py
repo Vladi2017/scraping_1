99503e8f5fa0db02687156cf46c7782af543a98d
VladiManolescu <mvmanol@yahoo.com>
2023-06-11 23:07:17 +0300

    Error - Missing scheme in request url.., see next commit
    (scrapyEnv1) PS C:\Users\mvman\projects2\Jobs\Gates_com\gates> scrapy crawl --output out1.csv:csv spider_1
    2023-06-11 22:58:27 [scrapy.utils.log] INFO: Scrapy 2.6.1 started (bot: gates)
    ....
    2023-06-11 22:58:28 [scrapy.core.engine] INFO: Spider opened
    2023-06-11 22:58:28 [scrapy.extensions.logstats] INFO: Crawled 0 pages (at 0 pages/min), scraped 0 items (at 0 items/min)
    2023-06-11 22:58:28 [scrapy.extensions.telnet] INFO: Telnet console listening on 127.0.0.1:6023
    2023-06-11 22:58:28 [scrapy.core.engine] ERROR: Error while obtaining start requests
    Traceback (most recent call last):
      File "C:\ProgramFilesVld_x64\anaconda3\envs\scrapyEnv1\lib\site-packages\scrapy\core\engine.py", line 150, in _next_request
        request = next(self.slot.start_requests)
      File "C:\ProgramFilesVld_x64\anaconda3\envs\scrapyEnv1\lib\site-packages\scrapy\spiders\__init__.py", line 64, in start_requests
        yield Request(url, dont_filter=True)
      File "C:\ProgramFilesVld_x64\anaconda3\envs\scrapyEnv1\lib\site-packages\scrapy\http\request\__init__.py", line 60, in __init__
        self._set_url(url)
      File "C:\ProgramFilesVld_x64\anaconda3\envs\scrapyEnv1\lib\site-packages\scrapy\http\request\__init__.py", line 108, in _set_url
        raise ValueError(f'Missing scheme in request url: {self._url}')
    ValueError: Missing scheme in request url: c:%5CUsers%5Cmvman%5Cprojects2%5CJobs%5CGates_com%5Ctest1%5Cequipment-clazz=Buses&vehicle-type=School+Buses&year=2017&make=International%2FNavistar&model=CE&engine=Cummins+ISB6.7+Diesel
    2023-06-11 22:58:28 [scrapy.core.engine] INFO: Closing spider (finished)
    2023-06-11 22:58:28 [scrapy.statscollectors] INFO: Dumping Scrapy stats:
    ....

--- spider_1.py
+++ spider_1.py
@@ -3,12 +3,13 @@ import scrapy
 import scrapy.http.response.html
 from scrapy.selector import Selector
 from scrapy.selector import SelectorList
+import urllib3
 
 # reference file: my:rpr:C:\Users\mvman\projects2\Jobs\Gates_com\Vld1.txt#1 , 23:08 6/9/2023
 class Spyder1Spider(scrapy.Spider):
     name = 'spider_1'
-    allowed_domains = [r'C:\Users\mvman\projects2\Jobs\Gates_com']
-    start_urls = [r"file:///C:\Users\mvman\projects2\Jobs\Gates_com\result_1.html", r"file:///C:\Users\mvman\projects2\Jobs\Gates_com\result_2.html"]
+    # allowed_domains = [r'C:\Users\mvman\projects2\Jobs\Gates_com']
+    start_urls = [r"C:\Users\mvman\projects2\Jobs\Gates_com\test1\equipment-clazz=Buses&vehicle-type=School+Buses&year=2017&make=International%2FNavistar&model=CE&engine=Cummins+ISB6.7+Diesel", r"C:\Users\mvman\projects2\Jobs\Gates_com\test1\equipment-clazz=Passenger+Cars+%26+Light+Trucks&vehicle-type=Light+Trucks&year=2022&make=BMW&model=X3&engine=6-Cyl.+3.0+L+Electric+Assist"]
     custom_settings = {
         "DOWNLOAD_DELAY" : "5",
         "CONCURRENT_REQUESTS_PER_DOMAIN" : "1",
@@ -17,9 +18,15 @@ class Spyder1Spider(scrapy.Spider):
     }
     # allowed_domains = ['www.gates.com']
     # start_urls = [r"https://www.gates.com/us/en/ymm/search/vehicle/result.html?equipment-clazz=Buses&vehicle-type=School+Buses&year=2017&make=International%2FNavistar&model=CE&engine=Cummins+ISB6.7+Diesel"]
+    # base_url = r"https://www.gates.com/us/en/ymm/search/vehicle/result.html?"
+    base_url = "C:\\Users\\mvman\\projects2\\Jobs\\Gates_com\\test1\\"  # dont support raw string bexause of last backslash..
 
     def parse(self, response: scrapy.http.response.html.HtmlResponse):
         print(f"{ctime()}: tag._1: received response.!")
+        url = response.url
+        queryStr = url.replace(self.base_url, "")
+        fcd: dict = urllib3.parse.parse_qs(queryStr) #1stColumsDictionay
+        for k in fcd.keys(): fcd[k] = fcd[k][0]
         apc = response.xpath('//ul[contains(@class, "gor-accordion nested-accordion")]') #{apc=AllProductsContainer (in the current page)}
         categs: SelectorList = apc[0].xpath("child::li") #{axis, works also without mo."[0]". categs = (the) categoriesList}
         categ: Selector = None  # for VSC hints.
@@ -41,11 +48,13 @@ class Spyder1Spider(scrapy.Spider):
                         if s:
                             comments = s
                             break
-                    yield {
+                    lcd = { #lastColumsDictionay
                         "Category" : categN,
                         "Subcategory" : scn,
                         "Application" : app,
                         "Product Type" : prType,
                         "Part#" : part,
-                        "Comments" : comments
-                    }
+                        "Comments" : comments,
+                        "Link" : url}
+                    url = ""
+                    yield fcd.update(lcd)
